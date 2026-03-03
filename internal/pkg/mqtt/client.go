a907189d1da08aa66b1fcf4a2a0e1fe5c8942fb3
FelixTing <felix@iotechsys.com>
2026-03-03 15:26:57 +0800

    EDX-6825 Add MQTT connection lifecycle event logging
    Signed-off-by: FelixTing <felix@iotechsys.com>

--- internal/pkg/mqtt/client.go
+++ internal/pkg/mqtt/client.go
@@ -1,6 +1,7 @@
 /********************************************************************************
  *  Copyright 2019 Dell Inc.
  *  Copyright (c) 2023 Intel Corporation
+ *  Copyright (c) 2026 IOTech Ltd
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  *
@@ -21,6 +22,7 @@ import (
 	"encoding/pem"
 	"errors"
 	"fmt"
+	"log"
 	"os"
 	"sync"
 	"time"
@@ -121,6 +123,9 @@ func (mc *Client) Connect() error {
 
 func (mc *Client) onConnectHandler(_ pahoMqtt.Client) {
 	optionsReader := mc.mqttClient.OptionsReader()
+
+	log.Printf("INFO: MQTT connection established (ClientID: %s, Broker: %s)", optionsReader.ClientID(),
+		optionsReader.Servers()[0])
 
 	mc.subscriptionMutex.Lock()
 	defer mc.subscriptionMutex.Unlock()
@@ -331,6 +336,14 @@ func createClientOptions(
 	clientOptions.CleanSession = clientConfiguration.CleanSession
 	clientOptions.SetAutoReconnect(clientConfiguration.AutoReconnect)
 	clientOptions.SetConnectTimeout(time.Duration(clientConfiguration.ConnectTimeout) * time.Second)
+	clientOptions.SetConnectionLostHandler(func(client pahoMqtt.Client, err error) {
+		log.Printf("WARNING: MQTT connection lost (ClientID: %s, Broker: %s), error: %v",
+			clientConfiguration.ClientId, clientConfiguration.BrokerURL, err)
+	})
+	clientOptions.SetReconnectingHandler(func(client pahoMqtt.Client, opts *pahoMqtt.ClientOptions) {
+		log.Printf("INFO: MQTT reconnecting (ClientID: %s, Broker: %s)", clientConfiguration.ClientId,
+			clientConfiguration.BrokerURL)
+	})
 	tlsConfiguration, err := pkg.GenerateTLSForClientClientOptions(
 		clientConfiguration.BrokerURL,
 		clientConfiguration.TlsConfigurationOptions,
